cc370ac0942e92017a492d9053ef8b08721db80a
mrivaldo21 <72621480+mrivaldo21@users.noreply.github.com>
2023-04-06 04:19:47 +0700

    Create Jawaban.js

---
+++ Jawaban.js
@@ -0,0 +1,5 @@
+      1. --
+       const myStr = "Hey, I'm 25 and she is 31";
+        console.log(myStr.replace("and", "&"));
+        const assSentences = "Mr.RED has a red and a green car";
+        console.log(assSentences.replace("red", "blue"));
